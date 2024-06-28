ac57765cb1dd2d99cfab8c68ab1690b1f158031a
root <usphow@gmail.com>
2024-06-28 06:36:40 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,7 @@ execute: fab -f 1-pack_web_static.py do_pack
 from datetime import datetime
 from fabric.api import *
 
+
 def do_pack():
     """
     making an archive on web_static folder
@@ -14,6 +15,8 @@ def do_pack():
 
     time = datetime.now()
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+
+
 local('mkdir -p versions')
 create = local('tar -cvzf versions/{} web_static'.format(archive))
 if create is not None:
